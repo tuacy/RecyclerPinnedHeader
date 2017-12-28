c56f6e78c6e15fbc5b5840f7580dca986c7b7d3e
tuacy <1007178106@qq.com>
2017-12-28 21:58:59 +0800

    little change

--- pinnedheader/src/main/java/com/tuacy/pinnedheader/PinnedHeaderAdapter.java
+++ pinnedheader/src/main/java/com/tuacy/pinnedheader/PinnedHeaderAdapter.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.RecyclerView;
 public abstract class PinnedHeaderAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
 
 	/**
-	 * 判断该positio对应的位置是要固定
+	 * 判断该position对应的位置是要固定
 	 *
 	 * @param position adapter position
 	 * @return true or false
